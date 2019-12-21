90b84987f3f95f1e980e414e6385fc7bb2fdbe53
ants <andreasvirkus@gmail.com>
2019-12-21 22:00:39 +0200

    Add basic styles and vue-typer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <typer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Typer from './components/Typer'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: { Typer }
 }
 </script>
 
 <style lang="scss">
+:root {
+  --highlight: tomato;
+  --content: #fefefe;
+  --background: #222;
+}
+
+/* Handicraft reset */
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+html {
+  box-sizing: border-box;
+}
+body,
+body * {
+  margin: 0;
+  padding: 0;
+}
+body {
+  min-height: 100vh;
+  height: 100%;
+  background-color: var(--highlight);
+  padding: 1rem;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  height: calc(100vh - 2rem);
+  width: 100%;
+  background-color: var(--background);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+body,
+button,
+input,
+textarea {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, Ubuntu;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+/* Global styles */
+h1 {
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+  color: var(--content);
+  font-weight: 700;
+}
+
+button {
+  display: block;
+  border: none;
+  background: none;
+  color: inherit;
+  font-size: inherit;
+}
+button[disabled],
+.cursor-disabled {
+  cursor: not-allowed;
+}
+label,
+button,
+.cursor-pointer {
+  cursor: pointer;
+}
+.break-all {
+  word-break: break-all;
+}
+.break-word {
+  word-break: break-word;
+}
+
+a {
+  text-decoration-style: dashed;
+  color: inherit;
+}
+a:hover {
+  text-decoration-style: solid;
+  text-decoration-width: 2px;
+}
+
+::placeholder {
+  color: var(--highlight);
+}
+::selection {
+  background-color: var(--highlight);
+  color: white;
 }
 </style>
---
+++ src/components/Typer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <vue-typer
+      :text="['Kanban', 'Kan{b}an', 'kanban;', 'Kan}an']"
+      :repeat="Infinity"
+      :shuffle="false"
+      initial-action="typing"
+      :pre-type-delay="70"
+      :type-delay="70"
+      :pre-erase-delay="1600"
+      :erase-delay="60"
+      erase-style="backspace"
+      :erase-on-complete="false"
+      caret-animation="smooth"
+    />
+  </div>
+</template>
+
+<script>
+import { VueTyper } from 'vue-typer'
+
+export default {
+  name: 'typer',
+  components: { VueTyper }
+}
+</script>
+
+<style lang="scss">
+.wrapper {
+  .vue-typer {
+    font-family: inherit;
+    font-size: 3rem;
+  }
+  .vue-typer .custom.char.typed {
+    color: white;
+  }
+  .vue-typer .custom.caret {
+    width: 6px;
+    background-color: white;
+  }
+}
+</style>
